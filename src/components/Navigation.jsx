3a77d9a8046e0d0da058c3d935bf2aa5c0b233d7
Mike Lee <pride81_@hotmail.com>
2024-01-04 17:44:34 +0900

    Added a picture

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import picture from './assets/Japan_Tokio_Skyline_und_Mt._Fuji.jpg';
 
 export default function Navigation () {
 
   return (
     <div className="sidebar">
-      <img className="sidebar_profile" src="https://i.pinimg.com/736x/28/33/2b/28332bde2d3dd35ee18f7e0c680dc9cb.jpg"></img>
+      {/* <img className="sidebar_profile" src="https://i.pinimg.com/736x/28/33/2b/28332bde2d3dd35ee18f7e0c680dc9cb.jpg"></img> */}
+      <img className="sidebar_profile" src={picture} alt="tokyo"></img>
       <h2>Mike's Realm</h2>
       <div>
         <ul>
