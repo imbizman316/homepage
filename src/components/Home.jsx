c0cedd814e7f00bc1bbac5fb4de7e606eb517577
Mike Lee <pride81_@hotmail.com>
2024-03-23 03:05:34 +0900

    Added my face in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Navigation from "./Navigation"
+import myface from "./assets/User_Photo_leeh7_1.jpg"
 
 export default function Home () {
   
@@ -8,9 +9,14 @@ export default function Home () {
     <div className="main-top">
       <Navigation />
       <div>
-        <p>Welcome to my website. My name is Mike.
-          My dream is to become a formiddable programmer.
+        <img src={myface} alt="Face" style={{width: '150px', borderRadius: '10%', border: '5px solid black'}}></img>
+        <p>My name is Mike and welcome to my website. 
+          Here you can find my footsteps in my career as well as my hobbies and interest.
         </p>
+        <p>
+          Although I stepped my foot in the game industry as an assistant overseas business manager, I realized that coding was where I fitted the best later on in my career.
+        </p>
+
       </div>
     </div>
 
