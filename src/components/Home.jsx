1049a83c4b617289a01922bb385f80584c923172
Mike Lee <pride81_@hotmail.com>
2024-04-16 23:33:00 +0900

    polished css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,32 @@
-import React from "react"
-import Navigation from "./Navigation"
-import myface from "./assets/User_Photo_leeh7_1.jpg"
+import React from "react";
+import Navigation from "./Navigation";
+import myface from "./assets/User_Photo_leeh7_1.jpg";
+import "../../../homepage/src/index.css";
 
-export default function Home () {
-  
+export default function Home() {
   return (
-
     <div className="main-top">
       <Navigation />
-      <div>
-        <img src={myface} alt="Face" style={{width: '150px', borderRadius: '10%', border: '5px solid black'}}></img>
-        <p>My name is Mike and welcome to my website. 
-          Here you can find my footsteps in my career as well as my hobbies and interest.
+      <div className="home">
+        <img
+          src={myface}
+          alt="Face"
+          style={{
+            width: "150px",
+            borderRadius: "10%",
+            border: "5px solid black",
+          }}
+        ></img>
+        <p>
+          My name is Mike and welcome to my website. Here you can find my
+          footsteps in my career as well as my hobbies and interest.
         </p>
         <p>
-          Although I stepped my foot in the game industry as an assistant overseas business manager, I realized that coding was where I fitted the best later on in my career.
+          Although I stepped my foot in the game industry as an assistant
+          overseas business manager, later on in my career, I realized that
+          coding was where I fitted the best.
         </p>
-
       </div>
     </div>
-
-  )
+  );
 }
